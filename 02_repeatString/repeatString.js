3f6f209ca64c3c862c1511987b2ac312da595da0
oshusha <adoshurkova@gmail.com>
2022-08-14 20:48:46 +0300

    Add solution of the 02_repeatString task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let res = '';
+    let j = 0;
 
+    if (num < 0) return 'ERROR';
+    
+    while (j < num) {
+        for (let i = 0; i < str.length; i++) {
+                res += str[i];
+            }
+        j = j + 1;
+    }
+    return res;
 };
 
 // Do not edit below this line
