e3749476b5579259ef412458698088b2ec8f1140
oshusha <adoshurkova@gmail.com>
2022-08-15 17:37:13 +0300

    Add a solution of the 05_sumAll task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startNum, finishNum) {
+    if (typeof startNum !== 'number' || typeof finishNum !== 'number') {
+        return 'ERROR';
+    }
 
+    if (startNum < 0 || finishNum < 0) return "ERROR";
+
+    if (startNum > finishNum) {
+        const temp = startNum;
+
+        startNum = finishNum;
+        finishNum = temp;
+    }
+    
+    let sum = 0;
+    for (let i = startNum; i <= finishNum; i++ ) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
